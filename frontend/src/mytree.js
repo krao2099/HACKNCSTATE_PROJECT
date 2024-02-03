289a589faa0535e4d69305cbd56365512502643c
lgaddy893 <lgaddy.lrhs@gmail.com>
2024-02-03 13:17:48 -0500

    Implemented tree package

---
+++ frontend/src/mytree.js
@@ -0,0 +1,37 @@
+import FamilyTree from "./familytree.js";
+import React, { Component } from 'react';
+        
+
+    export default class Chart extends Component {
+
+        constructor(props) {
+            super(props);
+            this.divRef = React.createRef();
+        }
+
+        shouldComponentUpdate() {
+            return false;
+        }
+
+        componentDidMount() {
+            this.family = new FamilyTree (this.divRef.current , {
+                nodes: this.props.nodes,
+
+                mouseScrool: FamilyTree.none,
+                siblingSeparation: 120,
+                template: 'john',
+                nodeBinding: {
+                    field_0: "name",
+                    field_1: "title",
+                    img_0: "img",
+                }
+            });
+        }
+
+        render() {
+            return (
+                <div id="tree" ref={this.divRef}></div>
+            );
+        }
+    }
+        
--- frontend/src/pages/Tree.js
+++ frontend/src/pages/Tree.js
@@ -1,15 +1,43 @@
 import React from 'react';
-import './Tree.css';
-import Person from '../components/Person';
+import './Tree.css'
+// import Person from '../components/Person';
+
+import FamilyTree from '../mytree';
+
 
 const Tree = () => {
-  return (
-    <div>
-      <div className='tree-header'>Family Tree</div>
-      <Person name="Jane Doe" birthday="2000-04-05" />
-      <Person name="John Smith" birthday="2001-09-23" />
-    </div>
-  );
+    return (
+        <div style={{height: '100%'}}>
+
+            <FamilyTree nodes={[
+                { id: 1, pids: [2], name: 'Amber McKenzie', gender: 'female', img: 'https://cdn.balkan.app/shared/2.jpg'  },
+                { id: 2, pids: [1], name: 'Ava Field', gender: 'male', img: 'https://cdn.balkan.app/shared/m30/5.jpg' },
+                { id: 3, mid: 1, fid: 2, name: 'Peter Stevens', gender: 'male', img: 'https://cdn.balkan.app/shared/m10/2.jpg' },
+                { id: 4, mid: 1, fid: 2, name: 'Savin Stevens', gender: 'male', img: 'https://cdn.balkan.app/shared/m10/1.jpg'  },
+                { id: 5, mid: 1, fid: 2, name: 'Emma Stevens', gender: 'female', img: 'https://cdn.balkan.app/shared/w10/3.jpg' }
+            ]} />
+        </div>
+    );
 };
 
 export default Tree;
+
+// import React, { Component } from 'react';
+//         import FamilyTree from './mytree';
+
+//         export default class App extends Component {
+//             render() {
+//                 return (
+//                     <div style={{height: '100%'}}>
+
+//                         <FamilyTree nodes={[
+//                             { id: 1, pids: [2], name: 'Amber McKenzie', gender: 'female', img: 'https://cdn.balkan.app/shared/2.jpg'  },
+//                             { id: 2, pids: [1], name: 'Ava Field', gender: 'male', img: 'https://cdn.balkan.app/shared/m30/5.jpg' },
+//                             { id: 3, mid: 1, fid: 2, name: 'Peter Stevens', gender: 'male', img: 'https://cdn.balkan.app/shared/m10/2.jpg' },
+//                             { id: 4, mid: 1, fid: 2, name: 'Savin Stevens', gender: 'male', img: 'https://cdn.balkan.app/shared/m10/1.jpg'  },
+//                             { id: 5, mid: 1, fid: 2, name: 'Emma Stevens', gender: 'female', img: 'https://cdn.balkan.app/shared/w10/3.jpg' }
+//                         ]} />
+//                     </div>
+//                 );
+//             }
+//         }
