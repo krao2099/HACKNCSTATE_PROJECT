359425ba913a4acf9b8bd7432f9d8fb947cb64e7
rpkenney <kenneyrobert109@gmail.com>
2024-02-03 14:03:10 -0500

    removed asyncs

--- server/app.py
+++ server/app.py
@@ -2,22 +2,21 @@ from flask import Flask, request, jsonify
 import os
 from user_profile import create_profile, view_profile
 from user_relationship import create_relationship, create_relationship_nojson
+from memory import create_memory, view_memory
 
 app = Flask(__name__)
 
 
 @app.route('/tree', methods=['GET'])
-async def retrieve_tree_endpoint():
+def retrieve_tree_endpoint():
     pass
 
-
-
 @app.route('/profile', methods=['GET'])
-async def view_profile_endpoint():
-    return jsonify(view_profile(request.data)), 200
+def view_profile_endpoint():
+    return jsonify(view_profile(request.json)), 200
 
 @app.route('/profile', methods=['POST'])
-async def create_profile_endpoint():
+def create_profile_endpoint():
     data = request.json
     id = create_profile(request.json)
     create_relationship_nojson(id, data['p1_id'], data['relation_type'])
@@ -27,22 +26,21 @@ async def create_profile_endpoint():
 
 
 @app.route('/relationship', methods=['GET'])
-async def view_relationship_endpoint():
+def view_relationship_endpoint():
     pass
 
 @app.route('/relationship', methods=['POST'])
-async def create_relationship_endpoint():
+def create_relationship_endpoint():
     pass
 
 @app.route('/memory', methods=['GET'])
-async def view_memory_endpoint():
-    pass
+def view_memory_endpoint():
+    data = request.json
+    return jsonify(view_memory(request.json)), 200
 
 @app.route('/relationship', methods=['POST'])
-async def create_memory_endpoint():
+def create_memory_endpoint():
     pass
-
-    
 
 @app.route('/test', methods=['GET'])
 def test():
