754c3a73145e8b5f908532d471023c1c11c2839a
rpkenney <kenneyrobert109@gmail.com>
2024-02-03 13:17:34 -0500

    added create_relationship

---
+++ server/user_relationship.py
@@ -0,0 +1,9 @@
+import database_util
+
+async def create_relationship(data):
+    p1_id = data['p1_id']
+    p2_id = data['p2_id']
+    type_id = data['type_id']
+
+    query = "INSERT INTO Relationship (p1_id, p2_id, type_id) VALUES (%s, %s, %s)"
+    database_util.execute(query, (p1_id, p2_id, type_id))
