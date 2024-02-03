9286b23fadf1c13ffe133695034ed340704cad55
klrao <getkiranrao@gmail.com>
2024-02-03 14:20:12 -0500

    removed last async

--- server/user_relationship.py
+++ server/user_relationship.py
@@ -1,6 +1,6 @@
 import database_util
 
-async def create_relationship_nojson(p1_id, p2_id, type_id):
+def create_relationship_nojson(p1_id, p2_id, type_id):
     query = "INSERT INTO Relationship (p1_id, p2_id, type_id) VALUES (%s, %s, %s) RETURNING id"
     return database_util.execute(query, (p1_id, p2_id, type_id), retrieve=True)
 
