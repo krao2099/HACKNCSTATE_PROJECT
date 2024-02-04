81ac29bfabe7c6d04fb7242ee2ed8a9d6f42d2be
rpkenney <kenneyrobert109@gmail.com>
2024-02-04 00:39:23 -0500

    worked on feed

--- frontend/src/components/FeedCard.js
+++ frontend/src/components/FeedCard.js
@@ -5,10 +5,7 @@ const FeedCard = ({ title, description,  people, file}) => {
 
   return (
     <div id = "card">
-        <div id="card_image">
-            <img src={file} alt="Memory photograph not found"></img>
-        </div>
-        <div id = "card-content">
+        <div id = "card_content">
             <h3 id = "title">{title}</h3>
             <div id = "people">
                 {people.map(p => (
@@ -16,6 +13,10 @@ const FeedCard = ({ title, description,  people, file}) => {
                 ))}
             </div>
             <p id = "description">{description}</p>
+        </div>
+
+        <div id="card_image">
+            <img src={file} alt="Memory photograph not found"></img>
         </div>
     </div>
   );
